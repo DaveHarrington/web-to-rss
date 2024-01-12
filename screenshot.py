8e9b1ce117b78c9ea642b0e7d28f2a2b7bc55bf8
Dave Harrington <harrington.dave@gmail.com>
2024-01-12 13:40:39 -0800

    initial commit

---
+++ screenshot.py
@@ -0,0 +1,113 @@
+import re
+import os
+import time
+import asyncio
+
+import dateparser
+from feedgenerator import Rss201rev2Feed
+from playwright.async_api import async_playwright, Playwright
+from dotenv import load_dotenv
+from datetime import datetime
+import pytz
+
+load_dotenv()
+
+save_tweets = []
+
+async def run(playwright: Playwright):
+
+    playwright = await async_playwright().start()
+    browser = await playwright.chromium.launch()
+    context = await browser.new_context(
+        user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/109.0',
+    )
+    twitter_cookies = [{
+        'name': 'auth_token',
+        'value': os.getenv("TWITTER_COOKIE"),
+        'domain': ".twitter.com",
+        'path': "/",
+    }]
+    await context.add_cookies(twitter_cookies)
+    page = await context.new_page()
+    await page.goto("https://twitter.com/mattyglesias")
+    await page.wait_for_selector('[data-testid="tweet"]')
+    time.sleep(5)
+    # Try to remove banner cruft
+    await page.evaluate("""
+() => {
+    document.querySelector('[data-testid=\"BottomBar\"]').remove()
+    try {
+        document.querySelector('[aria-label=\"sheetDialog\"]').parentNode.remove()
+    } catch(err) {
+    }
+}
+""")
+    tweets = page.locator('[data-testid="tweet"]')
+    count = await tweets.count()
+
+    for i in range(count):
+        tweet = tweets.nth(i)
+        all_t = await tweet.all_inner_texts()
+        contents = all_t[0].split("\n")
+
+        # Skip pinned tweets
+        if contents[0].startswith("Pinned"):
+            print("Skipped pinned tweet")
+            continue
+
+        user = contents[0]
+        handle = contents[1]
+        t_delta = contents[3]
+        tweet_txt = contents[4:-4]
+
+        tweet_html = await tweet.inner_html()
+        url_regex_pattern = r'href="(/[^"]*status[^"]*)"'
+        url_match = re.search(url_regex_pattern, tweet_html)
+        url = url_match.group(1)
+
+        datetime_regex_pattern = r'<time datetime="([^"]+)"'
+        datetime_match = re.search(datetime_regex_pattern, tweet_html)
+        if not datetime_match:
+            print("Skipped ad")
+            continue
+        datetime_part = datetime_match.group(1)
+        parsed_datetime = dateparser.parse(datetime_part)
+
+        # await tweet.screenshot(path=f"/tmp/screenshot-{i}.png")
+        save_tweets.append({
+            'user': user,
+            'handle': handle,
+            'datetime': parsed_datetime,
+            'tweet_txt': tweet_txt,
+            'link': url,
+        })
+
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+
+def parse_time_string(time_str):
+    return parsed_date
+
+def gen_feed(save_tweets):
+    # Initialize RSS feed
+    feed = Rss201rev2Feed(
+        title="Twitter RSS Feed",
+        link="http://twitter.com",
+        description="RSS feed for selected Twitter users",
+        language="en",
+    )
+    for t in save_tweets:
+        feed.add_item(
+            # title=f"{t.user}: {t.tweet_txt}",
+            title=t["user"],
+            link=t["link"],
+            description=t["tweet_txt"],
+            pubdate=t["datetime"],
+        )
+    return feed.writeString('utf-8')
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    with open("feed.xml", "w") as f:
+        f.write(gen_feed(save_tweets))
